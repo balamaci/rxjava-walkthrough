3493407e16aa756461db057bed483de5d0f16536
Serban Balamaci <serban.balamaci@1and1.ro>
2016-09-12 13:50:27 +0300

    more work on CreateObservable

---
+++ README.md
@@ -0,0 +1,19 @@
+## RxJava 1.x
+
+### [Part01CreateObservable]()
+
+#### Simple operators to create Observables
+
+```
+Observable<Integer> observable = Observable.just(1, 5, 10);
+Observable<Integer> observable = Observable.range(1, 10);
+
+```
+
+#### Creating your own Observable
+
+   Using Observable.create to handle the actual emissions of events
+
+``` 
+```
+ 
--- src/test/java/com/balamaci/rx/Part01CreateObservable.java
+++ src/test/java/com/balamaci/rx/Part01CreateObservable.java
@@ -1,9 +1,12 @@
 package com.balamaci.rx;
 
+import com.balamaci.rx.util.Helpers;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author sbalamaci
@@ -15,7 +18,7 @@ public class Part01CreateObservable implements BaseTestObservables {
 
     @Test
     public void just() {
-        Observable<Integer> observable = Observable.just(1,5,10);
+        Observable<Integer> observable = Observable.just(1, 5, 10);
 
         observable.subscribe(
                 val -> log.info("Subscriber received: {}", val));
@@ -23,15 +26,53 @@ public class Part01CreateObservable implements BaseTestObservables {
 
     @Test
     public void range() {
-        Observable<Integer> observable = Observable.range(1,10);
+        Observable<Integer> observable = Observable.range(1, 10);
+
+        observable.subscribe(
+                val -> log.info("Subscriber received: {}", val));
+    }
+
+    @Test
+    public void fromFuture() {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> { //starts a background task
+            log.info("CompletableFuture work starts");  //in the ForkJoin common pool
+            Helpers.sleepMillis(100);
+            return "red";
+        });
+
+        Observable<String> observable = Observable.from(completableFuture);
+
+        observable.subscribe(
+                val -> log.info("Subscriber received: {}", val));
+    }
+
+    @Test
+    public void fromArray() {
+        Observable<String> observable = Observable.from(new String[] {"red", "green", "blue", "black"});
 
         observable.subscribe(
                 val -> log.info("Subscriber received: {}"));
     }
 
+    /**
+     * Using Observable.create to handle the actual emissions of events with the events like onNext, onCompleted
+     *
+     * When using Observable.create you need to be aware of BackPressure and that Observables created with 'create'
+     * are not BackPressure aware
+     */
     @Test
     public void createSimpleObservable() {
-        Observable<Integer> observable = simpleObservable();
+        Observable<Integer> observable = Observable.create(subscriber -> {
+            log.info("Started emitting");
+
+            log.info("Emitting 1st");
+            subscriber.onNext(1);
+
+            log.info("Emitting 2nd");
+            subscriber.onNext(2);
+
+            subscriber.onCompleted();
+        });
 
         observable.subscribe(
                 val -> log.info("Subscriber received: {}", val),
@@ -40,6 +81,23 @@ public class Part01CreateObservable implements BaseTestObservables {
         );
     }
 
+    /**
+     * Event if we sleep for a long time inside create method(to simulate a costly operation,
+     * without subscribing to this Observable the code is not executed and the method returns immediately.
+     */
+    @Test
+    public void observablesAreLazy() {
+        Observable<Integer> observable = Observable.create(subscriber -> {
+            log.info("Started emitting but sleeping for 5 secs");
+            Helpers.sleepMillis(5000);
+            subscriber.onNext(1);
+        });
+    }
+
+    /**
+     * Inside the create() method, we can check is there are still active subscribers to our Observable.
+     * It's a way to stop emissions
+     */
     @Test
     public void showUnsubscribeObservable() {
         Observable<Integer> observable = Observable.create(subscriber -> {
@@ -52,16 +110,18 @@ public class Part01CreateObservable implements BaseTestObservables {
                     break;
                 }
             }
-
-            subscriber.onCompleted();
+            //subscriber.onCompleted(); to late to emit Complete event since subscriber already unsubscribed
         });
 
         observable
                 .take(5)
-                .subscribe(val -> log.info("Subscriber received: {}", val),
-                err -> log.error("Subscriber received error", err),
-                () -> log.info("Subscriber got Completed event")
+                .subscribe(
+                        val -> log.info("Subscriber received: {}", val),
+                        err -> log.error("Subscriber received error", err),
+                        () -> log.info("Subscriber got Completed event") //The Complete event is triggered by 'take()' operator
         );
     }
+
+
 
 }
